2d12899f57f78419a00f36e9ca7a5b67d788e6f6
RyanKopa <ryan.kopa@gmail.com>
2016-12-30 23:15:53 -0800

    set eval metric for xgb fit

--- airbnb.py
+++ airbnb.py
@@ -1,10 +1,10 @@
 #https://www.kaggle.com/c/airbnb-recruiting-new-user-bookings
 #
-#Python Model used to predict the classification of a user's next desired country to travel to, 
+#Python Model used to predict the classification of a user's next desired country to travel to,
 #given their activity on Airbnb.
 #
 #Input: train_users_2.csv and test_users.csv provided by Kaggle
-#Output: sub.csv, a csv file that lists the id of each user and a list of probabilities 
+#Output: sub.csv, a csv file that lists the id of each user and a list of probabilities
 #of the likeliness that the user will travel to a given country.
 
 import numpy as np
@@ -32,7 +32,8 @@ df_all = df_all.fillna(-1)
 #####Feature engineering#######
 #date_account_created
 #stack arrays vertically
-dac = np.vstack(df_all.date_account_created.astype(str).apply(lambda x: list(map(int, x.split('-')))).values)
+dac = np.vstack(df_all.date_account_created.astype(str).apply(
+    lambda x: list(map(int, x.split('-')))).values)
 df_all['dac_year'] = dac[:,0]
 df_all['dac_month'] = dac[:,1]
 df_all['dac_day'] = dac[:,2]
@@ -40,7 +41,9 @@ df_all['dac_day'] = dac[:,2]
 df_all = df_all.drop(['date_account_created'], axis=1)
 
 #timestamp_first_active
-tfa = np.vstack(df_all.timestamp_first_active.astype(str).apply(lambda x: list(map(int, [x[:4],x[4:6],x[6:8],x[8:10],x[10:12],x[12:14]]))).values)
+tfa = np.vstack(df_all.timestamp_first_active.astype(str).apply(
+    lambda x: list(map(int, [x[:4],x[4:6],x[6:8],x[8:10],
+    x[10:12],x[12:14]]))).values)
 df_all['tfa_year'] = tfa[:,0]
 df_all['tfa_month'] = tfa[:,1]
 df_all['tfa_day'] = tfa[:,2]
@@ -51,7 +54,10 @@ av = df_all.age.values
 df_all['age'] = np.where(np.logical_or(av<14, av>100), -1, av)
 
 #One-hot-encoding features
-ohe_feats = ['gender', 'signup_method', 'signup_flow', 'language', 'affiliate_channel', 'affiliate_provider', 'first_affiliate_tracked', 'signup_app', 'first_device_type', 'first_browser']
+ohe_feats = ['gender', 'signup_method', 'signup_flow', 'language',
+    'affiliate_channel', 'affiliate_provider',
+    'first_affiliate_tracked', 'signup_app', 'first_device_type',
+    'first_browser']
 for f in ohe_feats:
     df_all_dummy = pd.get_dummies(df_all[f], prefix=f)
     df_all = df_all.drop([f], axis=1)
@@ -63,7 +69,7 @@ X = vals[:piv_train]
 #normalizing labels (encode labels with values between 0 and n_classes-1)
 le = LabelEncoder()
 #Fit label encoder and return encoded labels
-y = le.fit_transform(labels) 
+y = le.fit_transform(labels)
 #finds values of dataframe at 213451 rows
 X_test = vals[piv_train:]
 
@@ -76,21 +82,28 @@ X_test = vals[piv_train:]
 
 #objective:  'multi:softprob'  = set XGBoost to do multiclass classification using the softmax objective,
 #			you also need to set number of classes
-#			output a vector of ndata * nclass, which can be further reshaped to ndata, nclass matrix. 
+#			output a vector of ndata * nclass, which can be further reshaped to ndata, nclass matrix.
 #			The result contains predicted probability of each data point belonging to each class.
 
 #subsample : float    Subsample ratio of the training instance.
 #colsample_bytree : float    Subsample ratio of columns when constructing each tree.
 #seed : int    Random number seed.
 
-xgb = XGBClassifier(max_depth=6, learning_rate=0.2, n_estimators=25,
-                    objective='multi:softprob', subsample=0.5, colsample_bytree=0.5, seed=0)                  
+xgb = XGBClassifier(max_depth=6,
+                    learning_rate=0.2,
+                    n_estimators=25,
+                    objective='multi:softprob',
+                    subsample=0.5,
+                    colsample_bytree=0.5,
+                    seed=0)
 #fits test values and and encoded labels
-xgb.fit(X, y)
+eval_set  = [(X,y)]
+
+xgb.fit(X, y, eval_set=eval_set, eval_metric = 'mlogloss')
 #predicts coresponding class labels in the case of classification
 #predicts the probability of a user belonging to a class (country)
 #outputs a numpy array of shape (n_samples, n_classes)
-y_pred = xgb.predict_proba(X_test)  
+y_pred = xgb.predict_proba(X_test)
 
 #Taking the 5 classes with highest probabilities
 ids = []  #list of ids
@@ -99,8 +112,10 @@ for i in range(len(id_test)):
     idx = id_test[i]
     ids += [idx] * 5
     #Transform array or sparse matrix X back to feature mappings.
-    cts += le.inverse_transform(np.argsort(y_pred[i])[::-1])[:5].tolist()
+    cts += le.inverse_transform(
+        np.argsort(y_pred[i])[::-1])[:5].tolist()
 
 #Generate submission
-sub = pd.DataFrame(np.column_stack((ids, cts)), columns=['id', 'country'])
+sub = pd.DataFrame(np.column_stack((ids, cts)), columns=['id',
+                                                        'country'])
 sub.to_csv('sub.csv',index=False)
